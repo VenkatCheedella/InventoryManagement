5137e177eec0ba422c73d9ccccaf173a7a44b201
venkat <venkatsubbu89@gmail.com>
2016-03-07 17:15:58 -0800

    Comments are provided to all packages

--- InventoryManagement/src/com/venkat/test/TestProductRequest.java
+++ InventoryManagement/src/com/venkat/test/TestProductRequest.java
@@ -1,5 +1,8 @@
 package com.venkat.test;
 
+/**
+ * An independent test to check whether the Order streams produces orders
+ */
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -24,7 +27,7 @@ public class TestProductRequest implements Runnable {
 	public void dataSource(){
 		int orderId =1;
 		for(int i=0; i < 5; ++i){
-			Order order = OrderGenerator.getOrder(RandomNumberGeneration.getRandomNumber(6), orderId);
+			Order order = OrderGenerator.getOrder(RandomNumberGeneration.getRandomNumber(6), orderId);		// generates an order based on the number of lines requested
 			System.out.print(id + " ");
 			System.out.println(order.toString());
 			System.out.println();
